fa7f9e0e58038b807751da9d257a192adaae6b28
= <mdabutahersaikat@gmail.com>
2022-06-01 20:56:01 +0600

    website is ready for adding redux

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import Products from '../components/Products'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+        <h2 className="heading">Welcome to the Redux toolkit store</h2>
+        <section>
+            <h3>Products</h3>
+            <Products />
+        </section>
+    </div>
   )
 }
 
